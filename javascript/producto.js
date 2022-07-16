03fdc2643d447d30c6ee30ee43bf1e86cfe83511
camila <camifarina15@gmail.com>
2022-07-15 21:35:58 -0300

    agregando ultimos detalles

--- javascript/producto.js
+++ javascript/producto.js
@@ -1,9 +1,12 @@
 
 let listaComponentes = [
-    {id: 1,cantidad:1, producto:'placa de video RTX2060', precio:80000,  img: './imagenes/placaRtx2060.jpg'},
-    {id: 2,cantidad:1, producto:'placa de video RTX3080', precio:300000, img: './imagenes/placaRTX3080.jpg' },
-    {id: 3,cantidad:1, producto:'placa de video GTX1050ti', precio:70000,img: './imagenes/placaGtx1050Ti.jpg'},
-    {id: 4,cantidad:1, producto:'procesador ryzen 3-3200', precio:80000, img: './imagenes/ryzen3-3200.jpg'},
-    {id: 5,cantidad:1, producto:'procesador ryzen 5-3600', precio:40000, img: './imagenes/ryzen5-3600.webp'},
-    {id: 6,cantidad:1, producto:'procesador ryzen 5-5600', precio:35000, img: './imagenes/ryzen5-5600.png'},
+    {id: 1,cantidad:1, producto:'Placa De Video RTX2060', precio:80000, img: './imagenes/placaRtx2060.jpg'},
+    {id: 2,cantidad:1, producto:'Placa De Video RTX3080', precio:300000, img: './imagenes/placaRTX3080.jpg' },
+    {id: 3,cantidad:1, producto:'Placa De Video GTX1050ti', precio:70000, img: './imagenes/placaGtx1050Ti.jpg'},
+    {id: 4,cantidad:1, producto:'Procesador Ryzen 3-3200', precio:80000, img: './imagenes/ryzen3-3200.jpg'},
+    {id: 5,cantidad:1, producto:'Procesador Ryzen 5-3600', precio:40000, img: './imagenes/ryzen5-3600.webp'},
+    {id: 6,cantidad:1, producto:'Procesador Ryzen 5-5600', precio:35000, img: './imagenes/ryzen5-5600.png'},
+    {id:7,cantidad:1, producto:'Placa Madre ASUS Micro ATX Prime B560M ', precio:80000, img:'./imagenes/placamadre-ASUS.jpg'},
+    {id:8,cantidad:1, producto:'Placa Madre ASUS Prime H510M ', precio:80000, img:'./imagenes/placamadre-ASUSPrimejpg'},
+    {id:9,cantidad:1, producto:'Placa Madre ASUS Prime A320M ', precio:80000, img:'./imagenes/placamadre-asusprime.jpg'},
 ]
